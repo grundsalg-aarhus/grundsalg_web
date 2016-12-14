845b1d87aa2860ee304fa9c92cfda4c0edffe66f
Martin Yde Granath <my@aarhus.dk>
2016-12-14 14:36:11 +0100

    Added tabs and media

--- web/modules/custom/grundsalg_tabs/js/app.js
+++ web/modules/custom/grundsalg_tabs/js/app.js
@@ -1,4 +1,34 @@
-var app = angular.module('grundsalgTabs', ['ngRoute']);
+var app = angular.module('grundsalgTabs', ['ngRoute', 'ngAnimate']);
+
+app.directive('slider', function() {
+  return {
+    restrict: 'AE',
+    replace: true,
+    scope: {
+      images: '='
+    },
+    link: function(scope) {
+      scope.currentIndex = 0;
+      scope.modulepath = drupalSettings.variables.app_dir + "/..";
+
+      scope.next = function() {
+        scope.currentIndex < scope.images.length-1 ? scope.currentIndex++ : scope.currentIndex = 0;
+      };
+
+      scope.prev = function() {
+        scope.currentIndex > 0 ? scope.currentIndex-- : scope.currentIndex = scope.images.length-1;
+      };
+
+      scope.$watch('currentIndex',function() {
+        scope.images.forEach(function(image) {
+          image.visible = false;
+        });
+        scope.images[scope.currentIndex].visible = true;
+      });
+    },
+    templateUrl: drupalSettings.variables.app_dir + '/templates/slideshowImage.html'
+  };
+});
 
 app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
   $locationProvider.hashPrefix('');
--- web/modules/custom/grundsalg_tabs/js/controllers/StreetviewController.js
+++ web/modules/custom/grundsalg_tabs/js/controllers/StreetviewController.js
@@ -1,6 +1,18 @@
 angular.module('grundsalgTabs').controller('StreetviewController', ['$scope', function($scope) {
-
-  // Set paths from the backend.
+  // Set coordiantes from the backend.
   $scope.coordinates = drupalSettings.variables.coordinates;
-
+  var panorama;
+  
+  function initialize() {
+    panorama = new google.maps.StreetViewPanorama(
+      document.getElementById('street-view'),
+      {
+        position: {lat: parseFloat(drupalSettings.variables.coordinates['lat']), lng: parseFloat(drupalSettings.variables.coordinates['lon'])},
+        pov: {heading: 165, pitch: 0},
+        zoom: 1
+      });
+  }
+  if ($scope.coordinates) {
+    initialize();
+  }
 }]);
