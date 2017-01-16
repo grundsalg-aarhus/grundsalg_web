27f0e2addf669f1c7d581b4db2619da6208e7232
Jesper Kristensen <cableman@linuxdev.dk>
2017-01-16 15:05:33 +0100

    Issue #GRUND-144: Started on new maps module

---
+++ web/modules/custom/grundsalg_maps/js/MapController.js
@@ -0,0 +1,10 @@
+/**
+ * @file
+ * Contains the Map Controller.
+ */
+
+angular.module('grundsalg').controller('MapController', ['$scope', function($scope) {
+  'use strict';
+
+  // @TODO: Why is this included? It does nothing.
+}]);
