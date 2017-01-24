90aa2c2f5ac427e99df593505c8bc9f2f32e7ce6
Jesper Kristensen <cableman@linuxdev.dk>
2017-01-24 10:19:59 +0100

    Issue #GRUND-200: Added block to display maps on overview pages

--- web/modules/custom/grundsalg_maps/js/MapController.js
+++ web/modules/custom/grundsalg_maps/js/MapController.js
@@ -3,50 +3,9 @@
  * Contains the Map Controller.
  */
 
-angular.module('grundsalg').controller('MapController', ['$scope', 'ticketService',
-  function($scope, ticketService) {
+angular.module('grundsalg').controller('MapController', ['$scope', 'ticketService', 'cookieService',
+  function($scope, ticketService, cookieService) {
     'use strict';
-
-    /**
-     * Cookie object.
-     *
-     * Used to handle the cookie(s), mainly used to store the connection JSON Web Token.
-     */
-    var Cookie = (function () {
-      return function (name) {
-        var self = this;
-
-        // Get token from the cookie.
-        self.get = function get() {
-          var regexp = new RegExp("(?:^" + name + "|\s*" + name + ")=(.*?)(?:;|$)", "g");
-          var result = regexp.exec(document.cookie);
-
-          return (result === null) ? undefined : result[1];
-        };
-
-        // Set token.
-        self.set = function set(value, expire) {
-          var cookie = name + '=' + escape(value) + ';';
-
-          // Default expire to one year.
-          if (expire === undefined) {
-            expire = new Date();
-            expire.setTime(expire.getTime() + (365*24*60*60*1000));
-          }
-          cookie += 'expires=' + expire.toUTCString() + ';';
-
-          cookie += 'path=/;';
-          cookie += 'domain=' + document.domain + ';';
-
-          document.cookie = cookie;
-        };
-
-        // Remove the cookie by expiring it.
-        self.remove = function remove() {
-          self.set('', 'Thu, 01 Jan 1970 00:00:00 GMT');
-        };
-      };
-    })();
 
     function displayMaps(kfticket) {
       proj4.defs("EPSG:25832","+proj=utm +zone=32 +ellps=GRS80 +units=m +no_defs");
@@ -139,19 +98,26 @@ angular.module('grundsalg').controller('MapController', ['$scope', 'ticketServic
       });
     }
 
-    var ticket_cookie = new Cookie('kfticket');
-    var ticket = ticket_cookie.get();
-    var expire = new Date();
-    expire.setTime(expire.getTime() + (23*60*60*1000));
+    var cookieName = 'kfticket';
+    var ticket = cookieService.get(cookieName);
 
     if (ticket == undefined) {
       ticketService.getTicket().then(function success(ticket) {
-        ticket_cookie.set(ticket, expire);
+        // Store the new ticket in the cookie for 23 hours.
+        var expire = new Date();
+        expire.setTime(expire.getTime() + (23*60*60*1000));
+        cookieService.set(cookieName, ticket, expire);
+
+        // Display the map.
         displayMaps(ticket);
       },
       function err(err) {
         console.error(err);
       });
+    }
+    else {
+      // Display the map.
+      displayMaps(ticket);
     }
   }
 ]);
--- web/modules/custom/grundsalg_maps/js/services/CookieService.js
+++ web/modules/custom/grundsalg_maps/js/services/CookieService.js
@@ -1,7 +1,62 @@
-
-angular.module('grundsalg').service('CookieService', ['$http',
-  function ($http) {
+/**
+ * @file
+ * Service to access cookies.
+ */
+angular.module('grundsalg').service('cookieService',
+  function () {
     'use strict';
 
+
+    /**
+     * Get cookie value.
+     *
+     * @param {string} name
+     *   The name of the cookie.
+     *
+     * @returns {undefined}
+     *   The contents of the cookie.
+     */
+    this.get = function get(name) {
+      var regexp = new RegExp("(?:^" + name + "|\s*" + name + ")=(.*?)(?:;|$)", "g");
+      var result = regexp.exec(document.cookie);
+
+      return (result === null) ? undefined : result[1];
+    };
+
+    /**
+     * Set value in a cookie.
+     *
+     * @param {string} name
+     *   The name of the cookie.
+     * @param {string} value
+     *   The value to set.
+     * @param {string} expire
+     *   The expire date as an UTCString.
+     */
+    this.set = function set(name, value, expire) {
+      var cookie = name + '=' + escape(value) + ';';
+
+      // Default expire to one year.
+      if (expire === undefined) {
+        expire = new Date();
+        expire.setTime(expire.getTime() + (365*24*60*60*1000));
+      }
+      cookie += 'expires=' + expire.toUTCString() + ';';
+
+      cookie += 'path=/;';
+      cookie += 'domain=' + document.domain + ';';
+
+      document.cookie = cookie;
+    };
+
+    /**
+     * Remove cookie.
+     *
+     * @param {string} name
+     *   The name of the cookie to remove.
+     */
+    this.remove = function remove(name) {
+      this.set(name, '', 'Thu, 01 Jan 1970 00:00:00 GMT');
+    };
   }
-]);
+);
