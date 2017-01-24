864483985bba16906b529b6be35271ae5dc0b97a
Jesper Kristensen <cableman@linuxdev.dk>
2017-01-24 22:16:30 +0100

    Issue #GRUND-217: Added Angular cache to mix

--- web/modules/custom/grundsalg_plots/js/services/PlotsService.js
+++ web/modules/custom/grundsalg_plots/js/services/PlotsService.js
@@ -3,18 +3,27 @@
  * Ticket service to generate ticket to access maps.
  */
 
-angular.module('grundsalg').service('plotsService', ['$http', '$q',
-  function ($http, $q) {
+angular.module('grundsalg').service('plotsService', ['$http', '$q', 'CacheFactory',
+  function ($http, $q, CacheFactory) {
     'use strict';
 
     var config = drupalSettings.grundsalg_plots;
 
+    // Create cache object (expire 10 min.).
+    // @TODO: Make cache configurable in "Site settings".
+    if (!CacheFactory.get('plotsCache')) {
+      CacheFactory.createCache('plotsCache', {
+        maxAge: 600 * 1000,
+        deleteOnExpire: 'aggressive',
+        storageMode: 'localStorage'
+      });
+    }
+    var plotsCache = CacheFactory.get('plotsCache');
 
     /**
      * Get plots from the server.
      *
      * @TODO: Handle geoJSON information.
-     * @TODO: Add cache.
      *
      * @param {number} subdivisionId
      *   The subdivision ID to fetch plots for.
@@ -24,16 +33,26 @@ angular.module('grundsalg').service('plotsService', ['$http', '$q',
 
       if (config && config.hasOwnProperty('url')) {
         var url = config.url.replace('%subdivisionId%', subdivisionId);
+        var cid = 'plots_' + subdivisionId;
 
-        $http({
-          method: 'GET',
-          url: url
-        }).then(function success(response) {
-          deferred.resolve(response.data);
-        }, function error(response) {
-          console.error(response);
-          deferred.reject('Error communicating with the server.');
-        });
+        var plots =  plotsCache.get(cid);
+        if (plots !== undefined) {
+          deferred.resolve(plots);
+        }
+        else {
+          $http({
+            method: 'GET',
+            url: url
+          }).then(function success(response) {
+            var plots = response.data;
+            plotsCache.put(cid, plots);
+
+            deferred.resolve(plots);
+          }, function error(response) {
+            console.error(response);
+            deferred.reject('Error communicating with the server.');
+          });
+        }
       }
       else {
         deferred.reject('No maps end-point URL in configuration');
